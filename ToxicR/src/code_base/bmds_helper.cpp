6877af67a866b5a1cd95819be45085aac98b16ae
Cody Simmons <simmons.cody@epa.gov>
2020-12-16 23:16:34 -0500

    Added BMDS helper routines for continuous models

--- ToxicR/src/code_base/bmds_helper.cpp
+++ ToxicR/src/code_base/bmds_helper.cpp
@@ -33,6 +33,28 @@ void calcDichoAIC(struct dichotomous_analysis *anal, struct dichotomous_model_re
   BMDSres->AIC = 2*(res->max + estParmCount);
 }
 
+void calcContAIC(struct continuous_analysis *anal, struct continuous_model_result *res, struct BMDS_results *BMDSres){
+
+  printf("LL=%f\n",res->max);
+  printf("model_df=%f\n",res->model_df);
+
+  bool freqModel = anal->prior[0] == 0;
+
+  // First find number of bounded parms
+   int bounded = checkForBoundedParms(anal->parms, res->parms, anal->prior, BMDSres);
+  
+  
+  double estParmCount = res->model_df - bounded;
+  //if freq then model_df should be rounded to nearest whole number
+  if (freqModel)
+    estParmCount = round(estParmCount);
+  
+    BMDSres->AIC = 2*(res->max + estParmCount);
+  
+  //               //    BMDSres->AIC = -9998.0;
+  
+}
+
 double findQuantileVals(double *quant, double *val, int arrSize, double target){
 
    double retVal = -9999.0;
@@ -70,5 +92,34 @@ void collect_dicho_bmd_values(struct dichotomous_analysis *anal, struct dichotom
 }
 
 void collect_cont_bmd_values(struct continuous_analysis *anal, struct continuous_model_result *res, struct BMDS_results *BMDSres){
+
+  int distSize = res->dist_numE*2;
+  printf("distSize: %d\n",distSize);
+
+  double dist[distSize/2][2];
+  double quant[distSize/2];
+  double val[distSize/2];
+
+  for (int i = 0; i < distSize/2; i++){
+    dist[i][1] = res->bmd_dist[i];
+    val[i] = res->bmd_dist[i];
+  }
+  for (int i = distSize/2; i < distSize; i++){
+    dist[i-distSize/2][0] = res->bmd_dist[i];
+    quant[i-distSize/2] = res->bmd_dist[i];
+  }
+
+  printf("dist:\n");
+  for (int i = 0; i < distSize/2; i++)
+  {
+     printf("quant:%f, val:%f\n",dist[i][0],dist[i][1]);
+     printf("quant:%f, val:%f\n",quant[i],val[i]);
+  }
+
+  calcContAIC(anal, res, BMDSres);
+  BMDSres->BMD = findQuantileVals(quant, val, distSize/2, 0.50);
+  BMDSres->BMDL = findQuantileVals(quant, val, distSize/2, 0.05);
+  BMDSres->BMDU = findQuantileVals(quant, val, distSize/2, 0.95);
+
   
 }
