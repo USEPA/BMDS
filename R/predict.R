f49c4186fa3041f917453ba61ea81aa66d57f609
Matt Wheeler <wheelemw@gmail.com>
2022-03-15 09:41:13 -0400

    Predict Continuous MCMC.

--- R/predict.R
+++ R/predict.R
@@ -110,9 +110,70 @@
         }
 
         if (grepl("Log-Normal",fit$full_model)){
-            returnV <- list(X = test_doses, Y = exp(log(as.numeric(f))+ 0.5*exp(fit$parameters[length(fit$parameters)]))) #lognormal mean 
+            returnV <- list(X = test_doses, Y = exp(log(as.numeric(f))+ 0.5*exp(fit$parameters[length(fit$parameters)])))#lognormal mean  # nolint
         }else{
             returnV <- list(X = test_doses, Y = as.numeric(f))
+        }
+        return(returnV)
+}
+
+
+.continuous_predict_model_mcmc <- function(fit, new_doses = NULL){
+
+        data_d = fit$data
+
+        if (ncol(data_d) == 4 ){ #sufficient statistics
+            mean <- data_d[,2,drop=F]
+            se   <- data_d[,4,drop=F]/sqrt(data_d[,3,drop=F])
+            doses <- data_d[,1,drop=F]
+            lm_fit = lm(mean ~ doses,weights = 1/(se*se))
+       }else{
+            Response <- data_d[,2,drop=F]
+            doses = data_d[,1,drop=F]
+            lm_fit = lm(Response~doses)
+       }
+
+        if (is.null(new_doses)){
+             test_doses = fit$data[,1]
+        }else{
+             test_doses = new_doses
+        }
+
+       if (coefficients(lm_fit)[2] < 0){
+         decrease = TRUE
+       }else{
+         decrease = FALSE
+       }
+
+        if (fit$model=="FUNL"){
+             f <- apply(fit$mcmc_result$PARM_samples, 1, cont_FUNL_f,test_doses)
+        }
+        if (fit$model=="hill"){
+              f <- apply(fit$mcmc_result$PARM_samples, 1,cont_hill_f,test_doses)
+        }
+        if (fit$model=="exp-3"){
+              f <- apply(fit$mcmc_result$PARM_samples, 1,cont_exp_3_f,test_doses)
+          }
+        if (fit$model=="exp-5"){
+             f <- apply(fit$mcmc_result$PARM_samples, 1,cont_exp_5_f,test_doses)
+        }
+        if (fit$model=="power"){
+              f <- apply(fit$mcmc_result$PARM_samples, 1,cont_power_f,test_doses)
+        }
+        if (fit$model=="polynomial"){
+             if (length(grep(": normal-ncv", tolower(fit$full_model)))>0){
+                degree = ncol(fit$mcmc_result$PARM_samples) - 2
+            }else{
+                degree = ncol(fit$mcmc_result$PARM_samples) - 1
+            }
+
+            f <- apply(fit$mcmc_result$PARM_samples[,1:degree], 1, cont_polynomial_f,test_doses)
+        }
+
+        if (grepl("Log-Normal",fit$full_model)){
+            returnV <- list(X = test_doses, Y = exp(log(f)+ 0.5*exp(fit$parameters[length(fit$parameters)])))#lognormal mean  # nolint
+        }else{
+            returnV <- list(X = test_doses, Y = f)
         }
         return(returnV)
 }
