a9ecd4903b6b689182864b297a191e3a3e74f54a
Andy Shapiro <shapiromatron@gmail.com>
2022-11-17 22:41:37 -0500

    updates?

--- tests/test_pybmds/test_bleep.py
+++ tests/test_pybmds/test_bleep.py
@@ -1,11 +1,10 @@
 import pybmds
-from pybmds import bleep
 
 
 def test_bleep():
     # test calling cpp extension directly
-    assert bleep.add(2, 2) == 4
-    assert bleep.sub(2, 2) == 0
+    assert pybmds.bleep.add(2, 2) == 4
+    assert pybmds.bleep.sub(2, 2) == 0
 
     # version -> python == cpp == expected value
     assert pybmds.bleep.__version__ == pybmds.__version__ == "0.0.1"
