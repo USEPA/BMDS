6c8d3170d87fb542edc70e8ed99f01e301641433
Andy Shapiro <shapiromatron@gmail.com>
2022-11-18 11:46:24 -0500

    Revert "Revert "move things to setup.py>""
    This reverts commit 121d36d16711c7a0338d5b24c9f0e4878f3bcd9d.

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from glob import glob
 
-from pybind11.setup_helpers import Pybind11Extension, build_ext
-from setuptools import setup
+from pybind11.setup_helpers import ParallelCompile, Pybind11Extension, build_ext, naive_recompile
+from setuptools import find_packages, setup
 
 __version__ = "0.0.1"
 
+ParallelCompile("NPY_NUM_BUILD_JOBS", needs_recompile=naive_recompile).install()
+
 setup(
+    name="pybmds",
     version=__version__,
     cmdclass={"build_ext": build_ext},
+    license="MIT",
+    install_requires=[
+        "typer",
+    ],
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "pybmds = pybmds.cli:app",
+        ]
+    },
     ext_modules=[
         Pybind11Extension(
             "pybmds.bleep",
