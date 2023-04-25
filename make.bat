e4c14b5a54cef4e64e3d335a809f492d3a2b7c1f
Andy Shapiro <shapiromatron@gmail.com>
2023-04-25 16:41:00 -0400

    add build/dist commands on windows

--- make.bat
+++ make.bat
@@ -7,6 +7,7 @@ if /I %1 == format goto :format
 if /I %1 == test goto :test
 if /I %1 == coverage goto :coverage
 if /I %1 == build goto :build
+if /I %1 == dist goto :dist
 goto :help
 
 :help
@@ -15,7 +16,8 @@ echo.  lint         perform both lint-py and lint-js
 echo.  format       perform both format-py and lint-js
 echo.  test         run python tests
 echo.  coverage     generate coverage report
-echo.  build        build a python wheel for distribution
+echo.  build        rebuild in development environment
+echo.  dist         build wheel package for distribution
 goto :eof
 
 :lint
@@ -36,6 +38,10 @@ coverage html
 goto :eof
 
 :build
+python setup.py develop
+goto :eof
+
+:dist
 rmdir /s /q .\build
 rmdir /s /q .\dist
 python setup.py bdist_wheel
