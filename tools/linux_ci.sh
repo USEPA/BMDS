462bf321d7753bd1d054be3869ac06a89402d6fe
Andy Shapiro <shapiromatron@gmail.com>
2023-04-27 13:55:15 -0400

    change dir

--- tools/linux_ci.sh
+++ tools/linux_ci.sh
@@ -3,8 +3,10 @@
 sudo apt-get update -y
 sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0 libgsl-dev libeigen3-dev libnlopt-dev
 
+/usr/bin/gsl-config
+
 export "EIGEN_DIR=/usr/include/eigen3/Eigen/"
-export "GSL_DIR=/usr/lib/x86_64-linux-gnu"
+export "GSL_DIR=/usr/include/gsl"
 export "NLOPT_DIR=/usr/include"
 export "CMAKE_C_COMPILER=/usr/bin/gcc-12"
 export "CMAKE_CXX_COMPILER=/usr/bin/g++-12"
