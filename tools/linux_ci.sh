0073a026d0359b686dc0c07406ca323982043605
Andy Shapiro <shapiromatron@gmail.com>
2023-04-27 16:19:24 -0400

    github action - linux (#11)
    * setup workspace
    
    * sudo
    
    * again
    
    * again
    
    * fix parens
    
    * gsl?
    
    * change dir
    
    * config
    
    * ?
    
    * another
    
    * ??
    
    * d
    
    * remove override
    
    * let cmake do its thing
    
    * nlopt
    
    * add eigen
    
    * cpp
    
    * add manual trigger

---
+++ tools/linux_ci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0 libgsl-dev libeigen3-dev libnlopt-dev libnlopt-cxx-dev
+
+export "EIGEN_DIR=/usr/include/eigen3"
+export "NLOPT_DIR=/usr/lib/x86_64-linux-gnu/"
+export "CMAKE_C_COMPILER=/usr/bin/gcc-12"
+export "CMAKE_CXX_COMPILER=/usr/bin/g++-12"
+export "PYTHON_EXECUTABLE=/opt/hostedtoolcache/Python/3.11.3/x64/bin/python"
+export "PYTHON_LIBRARY_DIR=/opt/hostedtoolcache/Python/3.11.3/x64/lib/python3.11/site-packages"
