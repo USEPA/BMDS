81e30a6c44dd98b93e82a8caaca2d0c225f51523
Andy Shapiro <shapiromatron@gmail.com>
2023-04-27 13:32:34 -0400

    gsl?

--- tools/linux_ci.sh
+++ tools/linux_ci.sh
@@ -4,14 +4,9 @@ sudo apt-get update -y
 sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0 libgsl-dev libeigen3-dev libnlopt-dev
 
 export "EIGEN_DIR=/usr/include/eigen3/Eigen/"
-export "GSL_DIR=/usr/include/gsl"
+export "GSL_DIR=/usr/lib/x86_64-linux-gnu"
 export "NLOPT_DIR=/usr/include"
 export "CMAKE_C_COMPILER=/usr/bin/gcc-12"
 export "CMAKE_CXX_COMPILER=/usr/bin/g++-12"
 export "PYTHON_EXECUTABLE=/opt/hostedtoolcache/Python/3.11.3/x64/bin/python"
-export "PYTHON_LIBRARY_DIR=/opt/hostedtoolcache/Python/3.11.3/x64/lib/site-packages"
-
-echo "here"
-python -m site
-echo "there"
-
+export "PYTHON_LIBRARY_DIR=/opt/hostedtoolcache/Python/3.11.3/x64/lib/python3.11/site-packages"
