e42fd87815002a5e62f2b023870dee20edea1c89
Andy Shapiro <shapiromatron@gmail.com>
2023-04-27 14:20:35 -0400

    let cmake do its thing

--- tools/linux_ci.sh
+++ tools/linux_ci.sh
@@ -3,16 +3,8 @@
 sudo apt-get update -y
 sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0 libgsl-dev libeigen3-dev libnlopt-dev
 
-echo "dpkg"
-dpkg -L libgsl-dev
-
-echo "ls"
-ls -l /usr/include/gsl/
-ls -l /usr/include/eigen3
-ls -l /usr/include/eigen3/Eigen
-
-export "EIGEN_DIR=/usr/include/eigen3"
-export "NLOPT_DIR=/usr/include"
+#export "EIGEN_DIR=/usr/include/eigen3"
+#export "NLOPT_DIR=/usr/include"
 export "CMAKE_C_COMPILER=/usr/bin/gcc-12"
 export "CMAKE_CXX_COMPILER=/usr/bin/g++-12"
 export "PYTHON_EXECUTABLE=/opt/hostedtoolcache/Python/3.11.3/x64/bin/python"
