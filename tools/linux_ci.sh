94dc2fdd58f3ac254227c34113cf025d2ce8a24e
Andy Shapiro <shapiromatron@gmail.com>
2023-04-27 15:05:03 -0400

    cpp

--- tools/linux_ci.sh
+++ tools/linux_ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0 libgsl-dev libeigen3-dev libnlopt-dev
+sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0 libgsl-dev libeigen3-dev libnlopt-dev libnlopt-cxx-dev
 
 export "EIGEN_DIR=/usr/include/eigen3"
 export "NLOPT_DIR=/usr/lib/x86_64-linux-gnu/"
