8be66bf3b91597a736628f0b526bc03e73217c74
Andy Shapiro <shapiromatron@gmail.com>
2024-05-30 11:51:39 -0400

    add back commands

--- tools/linux_ci.sh
+++ tools/linux_ci.sh
@@ -3,7 +3,9 @@
 sudo apt-get update -y
 sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0 libgsl-dev libeigen3-dev libnlopt-dev libnlopt-cxx-dev lcov
 
-export "EIGEN_DIR=/usr/include/eigen3"
-export "NLOPT_DIR=/usr/lib/x86_64-linux-gnu/"
-export "CMAKE_C_COMPILER=/usr/bin/gcc-12"
-export "CMAKE_CXX_COMPILER=/usr/bin/g++-12"
+export EIGEN_DIR="/usr/include/eigen3"
+export NLOPT_DIR="/usr/lib/x86_64-linux-gnu/"
+export CMAKE_C_COMPILER="/usr/bin/gcc-12"
+export CMAKE_CXX_COMPILER="/usr/bin/g++-12"
+export PYTHON_EXECUTABLE=$(which python)
+export PYTHON_LIBRARY_DIR=$(python -c "import site; print(site.getsitepackages()[0])")
