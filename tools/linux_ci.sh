ee129949cb1caabdf9c33f02777e3874ce42e243
Andy Shapiro <shapiromatron@gmail.com>
2024-05-30 13:01:59 -0400

    switch versions?

--- tools/linux_ci.sh
+++ tools/linux_ci.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0
 
 export EIGEN_DIR="/usr/include/eigen3"
 export NLOPT_DIR="/usr/lib/x86_64-linux-gnu/"
-export CMAKE_C_COMPILER="/usr/bin/gcc-12"
-export CMAKE_CXX_COMPILER="/usr/bin/g++-12"
+export CMAKE_C_COMPILER="/usr/bin/gcc-11"
+export CMAKE_CXX_COMPILER="/usr/bin/g++-11"
 export PYTHON_EXECUTABLE=$(which python)
 export PYTHON_LIBRARY_DIR=$(python -c "import site; print(site.getsitepackages()[0])")
