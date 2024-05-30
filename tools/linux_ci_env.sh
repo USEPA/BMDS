abc2b99396bd9ce185ee0ffb815c5dd6e1943438
Andy Shapiro <shapiromatron@gmail.com>
2024-05-30 17:26:23 -0400

    split up enviro commands

--- tools/linux_ci_env.sh
+++ tools/linux_ci_env.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0 libgsl-dev libeigen3-dev libnlopt-dev libnlopt-cxx-dev lcov
-
 export EIGEN_DIR="/usr/include/eigen3"
 export NLOPT_DIR="/usr/lib/x86_64-linux-gnu/"
 export CMAKE_C_COMPILER="/usr/bin/gcc-11"
