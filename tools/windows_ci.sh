868a34f52c7aaf5618f491d6259f5c6516bb90bd
Andy Shapiro <shapiromatron@gmail.com>
2024-05-30 21:26:50 -0400

    vendor libraries

---
+++ tools/windows_ci.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -xe
+
+# filenames and versions
+EIGEN="eigen-3.4.0"
+NLOPT="nlopt-2.7.1"
+GSL="ampl-gsl-60539d2"
+
+# only do this first time
+DEPS="$GITHUB_WORKSPACE/deps"
+if [ -d "$DEPS" ]; then
+    exit 0
+else
+    mkdir -p $DEPS
+fi
+
+# eigen
+cd $GITHUB_WORKSPACE
+cp ./vendor/$EIGEN.tar.gz $DEPS
+cd $DEPS && tar -xf $EIGEN.tar.gz
+
+# nlopt
+cd $GITHUB_WORKSPACE
+cp ./vendor/$NLOPT.tar.gz $DEPS
+cd $DEPS && tar -xf $NLOPT.tar.gz
+cd $NLOPT && mkdir build && cd build && cmake -DBUILD_SHARED_LIBS=OFF .. && cmake --build . --config Release
+
+# gsl
+cd $GITHUB_WORKSPACE
+cp ./vendor/$GSL.tar.gz $DEPS
+cd $DEPS && tar -xf $GSL.tar.gz
+cd $GSL && mkdir build && cd build && cmake .. -DNO_AMPL_BINDINGS=1 && cmake --build . --config Release
