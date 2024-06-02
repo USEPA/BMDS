f81cf1bd43f8e560e487f0890ab3d9ddb1369004
Andy Shapiro <shapiromatron@gmail.com>
2024-06-02 14:28:58 -0400

    commit

---
+++ tools/cibuildwheel-linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -xe
+
+source ./tools/linux_ci_setup.sh
+source ./tools/linux_ci_env.sh
--- tools/linux_ci_env.sh
+++ tools/linux_ci_env.sh
@@ -4,6 +4,4 @@ export EIGEN_DIR="/usr/include/eigen3"
 export NLOPT_DIR="/usr/lib/x86_64-linux-gnu/"
 export CMAKE_C_COMPILER="/usr/bin/gcc-11"
 export CMAKE_CXX_COMPILER="/usr/bin/g++-11"
-export PYTHON_EXECUTABLE=$(which python)
-export PYTHON_LIBRARY_DIR=$(python -c "import site; print(site.getsitepackages()[0])")
 export TEST_EXC="'/usr/include/*'"
