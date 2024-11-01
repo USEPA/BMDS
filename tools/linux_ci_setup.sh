29fe604cb348bb76c325b46dadb186cbb26eda64
Andy Shapiro <shapiromatron@gmail.com>
2024-11-01 11:36:20 -0400

    try 18

--- tools/linux_ci_setup.sh
+++ tools/linux_ci_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0 libgsl-dev libeigen3-dev libnlopt-dev libnlopt-cxx-dev lcov clang-format-19
+sudo apt-get install -y automake build-essential libtool make cmake libgslcblas0 libgsl-dev libeigen3-dev libnlopt-dev libnlopt-cxx-dev lcov clang-format-18
