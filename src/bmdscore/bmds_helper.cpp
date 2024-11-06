6f5a5744cf7a94c5c684fbae33fdf3d4b45ebc1d
Cody Simmons <simmons.cody@epa.gov>
2024-11-06 14:42:30 -0500

    Nlog bugfix (#77)
    * Added check for negative values in GSL calls
    
    * formatting
    
    * re-added code removed by merge
    
    * more formatting

--- src/bmdscore/bmds_helper.cpp
+++ src/bmdscore/bmds_helper.cpp
@@ -3269,6 +3269,8 @@ void Nlogist_Bootstrap(
                 Ep[j] * (1.0 - Ep[j]) / (Ep[j] * (1.0 - Ep[j]) * phi[Xg[j] - 1]) - 1.0;
             a = Ep[j] * tempvalue;
             b = (1.0 - Ep[j]) * tempvalue;
+            validatePositiveInput(a);
+            validatePositiveInput(b);
             cutpoint = gsl_ran_beta(r, a, b);
           }
         }
@@ -3289,6 +3291,8 @@ void Nlogist_Bootstrap(
             while (x <= 1.0 && cum_beta > cum_beta_comp) {
               cutpoint = x;
               x += cum_beta_step;
+              validatePositiveInput(a);
+              validatePositiveInput(b);
               cum_beta_comp = gsl_cdf_beta_P(x, a, b);
             }
           } else {  // random value is closer to high end
@@ -3298,6 +3302,8 @@ void Nlogist_Bootstrap(
             while (x >= 0.0 && cum_beta < cum_beta_comp) {
               x -= cum_beta_step;
               cutpoint = x;
+              validatePositiveInput(a);
+              validatePositiveInput(b);
               cum_beta_comp = gsl_cdf_beta_P(x, a, b);
             }
           }
@@ -3390,6 +3396,12 @@ void Nlogist_Bootstrap(
   pyRes->boot.perc99[BSLoops] = pctTemp[3];
 
   gsl_rng_free(r);  // free memory from random generator
+}
+
+void validatePositiveInput(double &val) {
+  if (val <= 0) {
+    val = BMDS_EPS;
+  }
 }
 
 void Nlogist_SRoI(
