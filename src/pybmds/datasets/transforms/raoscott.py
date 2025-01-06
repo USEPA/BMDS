e6af6575cdb5386cee2d5dd0b1ba66120ae4e67c
Andy Shapiro <shapiromatron@gmail.com>
2025-01-05 23:42:58 -0500

    update implementation

--- src/pybmds/datasets/transforms/raoscott.py
+++ src/pybmds/datasets/transforms/raoscott.py
@@ -1,4 +1,5 @@
 from enum import StrEnum
+from typing import ClassVar, NamedTuple
 
 import numpy as np
 import pandas as pd
@@ -7,9 +8,9 @@ from ..dichotomous import DichotomousDataset
 
 
 class Species(StrEnum):
-    rat = "Rat"
-    mouse = "Mouse"
-    rabbit = "Rabbit"
+    rat = "rat"
+    mouse = "mouse"
+    rabbit = "rabbit"
 
 
 class Design(StrEnum):
@@ -17,30 +18,47 @@ class Design(StrEnum):
     orthogonal = "Orthogonal"
 
 
-params = {
-    (Species.mouse, Design.least_square): (1.5938, 0.2866, 0.2078),
-    (Species.mouse, Design.orthogonal): (1.6943, 0.3132, 0.1863),
-    (Species.rat, Design.least_square): (1.6852, 0.331, 0.1248),
-    (Species.rat, Design.orthogonal): (1.8327, 0.369, 0.109),
-    (Species.rabbit, Design.least_square): (1.0582, 0.2397, 0.1452),
-    (Species.rabbit, Design.orthogonal): (1.1477, 0.2739, 0.1299),
-}
+class P(NamedTuple):
+    a: float
+    b: float
+    sigma: float
 
 
-def rao_scott(dataset: DichotomousDataset, species: Species) -> pd.DataFrame:
-    df = pd.DataFrame(
-        {
-            "dose": dataset.doses,
-            "incidence": dataset.incidences,
-            "n": dataset.ns,
-        }
-    )
-    df["fraction_affected"] = df.incidence / df.n
-    a, b, sigma = params[(species, Design.least_square)]
-    df["ls_design"] = np.exp(a + (b * np.log(df.fraction_affected)) + (0.5 * sigma))
-    a, b, sigma = params[(species, Design.orthogonal)]
-    df["o_design"] = np.exp(a + (b * np.log(df.fraction_affected)) + (0.5 * sigma))
-    df["avg_design"] = df[["ls_design", "o_design"]].mean(axis=1)
-    df["scaled_incidence"] = df.incidence / df.avg_design
-    df["scaled_n"] = df.n / df.avg_design
-    return df
+class RaoScott:
+    adjustment_parameters: ClassVar = {
+        (Species.mouse, Design.least_square): P(1.5938, 0.2866, 0.2078),
+        (Species.mouse, Design.orthogonal): P(1.6943, 0.3132, 0.1863),
+        (Species.rat, Design.least_square): P(1.6852, 0.331, 0.1248),
+        (Species.rat, Design.orthogonal): P(1.8327, 0.369, 0.109),
+        (Species.rabbit, Design.least_square): P(1.0582, 0.2397, 0.1452),
+        (Species.rabbit, Design.orthogonal): P(1.1477, 0.2739, 0.1299),
+    }
+
+    def __init__(self, dataset: DichotomousDataset, species: Species):
+        self.dataset = dataset
+        self.species = species
+        self.df = self.calculate()
+
+    def calculate(self) -> pd.DataFrame:
+        df = pd.DataFrame(
+            {
+                "dose": self.dataset.doses,
+                "incidence": self.dataset.incidences,
+                "n": self.dataset.ns,
+            }
+        )
+        df["fraction_affected"] = df.incidence / df.n
+
+        p = self.adjustment_parameters[(self.species, Design.least_square)]
+        df["ls_design"] = np.exp(p.a + (p.b * np.log(df.fraction_affected)) + (0.5 * p.sigma))
+
+        p = self.adjustment_parameters[(self.species, Design.orthogonal)]
+        df["o_design"] = np.exp(p.a + (p.b * np.log(df.fraction_affected)) + (0.5 * p.sigma))
+
+        df["avg_design"] = df[["ls_design", "o_design"]].mean(axis=1)
+        df["scaled_incidence"] = df.incidence / df.avg_design
+        df["scaled_n"] = df.n / df.avg_design
+        return df
+
+    def to_df(self) -> pd.DataFrame:
+        return self.df
