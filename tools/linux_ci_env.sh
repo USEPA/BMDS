fdfb19dacafcdd4b9dd29dcbc928f3d5ec79db45
Andy Shapiro <shapiromatron@gmail.com>
2025-07-30 00:40:21 -0400

    cleanup

--- tools/linux_ci_env.sh
+++ tools/linux_ci_env.sh
@@ -2,6 +2,6 @@
 
 export EIGEN_DIR="/usr/include/eigen3"
 export NLOPT_DIR="/usr/lib/x86_64-linux-gnu/"
-export CMAKE_C_COMPILER="/usr/bin/gcc-13"
-export CMAKE_CXX_COMPILER="/usr/bin/g++-13"
+export CMAKE_C_COMPILER="/usr/bin/gcc-14"
+export CMAKE_CXX_COMPILER="/usr/bin/g++-14"
 export TEST_EXC="'/usr/include/*'"
