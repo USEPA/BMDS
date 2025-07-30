059a7fd2912b805bda536423c12888c6750b25de
Andy Shapiro <shapiromatron@gmail.com>
2025-07-30 00:08:00 -0400

    fix nlopt name

--- tools/cibuildwheel-linux.sh
+++ tools/cibuildwheel-linux.sh
@@ -5,6 +5,8 @@ set -xe
 yum update -y
 yum install -y cmake gsl-devel eigen3-devel
 
+NLOPT="nlopt-2.10.0"
+
 cp ./vendor/$NLOPT.tar.gz ~
 cd ~
 tar -xf $NLOPT.tar.gz && cd $NLOPT && mkdir build && cd build && cmake -DBUILD_SHARED_LIBS=OFF .. && make install
