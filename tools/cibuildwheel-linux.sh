2b8bc4ed8cd5c5dabcb15eddf9ecaba2078ca2eb
Andy Shapiro <shapiro.andy@epa.gov>
2025-08-04 17:11:59 -0400

    fix minimum cmake version?

--- tools/cibuildwheel-linux.sh
+++ tools/cibuildwheel-linux.sh
@@ -7,5 +7,5 @@ yum install -y cmake gsl-devel eigen3-devel
 
 cp ./vendor/nlopt-2.7.1.tar.gz ~
 cd ~
-tar -xf nlopt-2.7.1.tar.gz && cd nlopt-2.7.1 && mkdir build && cd build && cmake -DBUILD_SHARED_LIBS=OFF .. && make install
+tar -xf nlopt-2.7.1.tar.gz && cd nlopt-2.7.1 && mkdir build && cd build && cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_POLICY_VERSION_MINIMUM=3.15 .. && make install
 cd $GITHUB_WORKSPACE
