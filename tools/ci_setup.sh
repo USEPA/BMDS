b1c57d5674eb96c49a6d0ad41673ce319f677290
Andy Shapiro <shapiro.andy@epa.gov>
2025-08-05 12:17:59 -0400

    linux setup

---
+++ tools/ci_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$RUNNER_OS" == "Linux" ]; then
+
+    # install dependencies
+    sudo apt-get update -y
+    sudo apt-get install -y automake build-essential libtool make cmake lcov cloc
+    if [ "$generateDocx" = "true" ]; then
+        sudo apt-get install -y pandoc
+    fi
+
+    # set export locations
+    export EIGEN_DIR="$GITHUB_WORKSPACE/vcpkg_installed/x64-linux-dynamic/include/eigen3"
+    export NLOPT_DIR="$GITHUB_WORKSPACE/vcpkg_installed/x64-linux-dynamic/lib"
+    export GSL_DIR="$GITHUB_WORKSPACE/vcpkg_installed/x64-linux-dynamic"
+    export CMAKE_C_COMPILER="/usr/bin/gcc-11"
+    export CMAKE_CXX_COMPILER="/usr/bin/g++-11"
+
+fi
