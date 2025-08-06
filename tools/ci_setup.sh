31703d9c9c60d7c0f85b5e6fcfc8903701050cd8
Andy Shapiro <shapiro.andy@epa.gov>
2025-08-06 10:35:03 -0400

    use build compiler

--- tools/ci_setup.sh
+++ tools/ci_setup.sh
@@ -6,7 +6,7 @@ set -xe
 if [ "$RUNNER_OS" == "Linux" ]; then
     # install dependencies
     sudo apt-get update -y
-    sudo apt-get install -y build-essential libtool cmake lcov cloc
+    sudo apt-get install -y lcov cloc
     if [ "$generateDocx" = "true" ]; then
         sudo apt-get install -y pandoc
     fi
@@ -15,8 +15,6 @@ if [ "$RUNNER_OS" == "Linux" ]; then
     export EIGEN_DIR="$GITHUB_WORKSPACE/vcpkg_installed/x64-linux-dynamic/include/eigen3"
     export NLOPT_DIR="$GITHUB_WORKSPACE/vcpkg_installed/x64-linux-dynamic/lib"
     export GSL_DIR="$GITHUB_WORKSPACE/vcpkg_installed/x64-linux-dynamic"
-    export CMAKE_C_COMPILER="/usr/bin/gcc-11"
-    export CMAKE_CXX_COMPILER="/usr/bin/g++-11"
     export CMAKE_BUILD_PARALLEL_LEVEL="$(nproc)"
     echo CMAKE_BUILD_PARALLEL_LEVEL="$CMAKE_BUILD_PARALLEL_LEVEL"
 
@@ -24,8 +22,6 @@ if [ "$RUNNER_OS" == "Linux" ]; then
     echo EIGEN_DIR="$EIGEN_DIR" >> $GITHUB_ENV
     echo NLOPT_DIR="$NLOPT_DIR" >> $GITHUB_ENV
     echo GSL_DIR="$GSL_DIR" >> $GITHUB_ENV
-    echo CMAKE_C_COMPILER="$CMAKE_C_COMPILER" >> $GITHUB_ENV
-    echo CMAKE_CXX_COMPILER="$CMAKE_CXX_COMPILER" >> $GITHUB_ENV
     echo CMAKE_BUILD_PARALLEL_LEVEL="$CMAKE_BUILD_PARALLEL_LEVEL" >> $GITHUB_ENV
 fi
 
