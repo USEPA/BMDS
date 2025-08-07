52e58584272ad668d407053cdc0a57632cada962
Andy Shapiro <shapiro.andy@epa.gov>
2025-08-07 13:34:50 -0400

    use cached builds

--- tools/cibw_setup.sh
+++ tools/cibw_setup.sh
@@ -6,16 +6,11 @@ set -e
 env
 
 if [ "$RUNNER_OS" == "Linux" ]; then
-./vcpkg/bootstrap-vcpkg.sh
-./vcpkg/vcpkg install --overlay-ports="./vendor/ports" --host-triplet="x64-linux-dynamic"
 fi
 
 
 if [ "$RUNNER_OS" == "Windows" ]; then
-
 fi
 
-
 if [ "$RUNNER_OS" == "macOS" ]; then
-
 fi
