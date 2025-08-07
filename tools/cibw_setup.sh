18cd704a93d86f488e8a8a7d7996e52cf1d9d716
Andy Shapiro <shapiro.andy@epa.gov>
2025-08-07 14:17:20 -0400

    remove quote

--- tools/cibw_setup.sh
+++ tools/cibw_setup.sh
@@ -16,5 +16,5 @@ ls -laht /project/vcpkg_installed/x64-linux-dynamic/include | true
 yum update -y
 yum install -y zip
 
-./vcpkg/bootstrap-vcpkg.sh"
+./vcpkg/bootstrap-vcpkg.sh
 ./vcpkg/vcpkg install --overlay-ports="${{ github.workspace }}/vendor/ports" --host-triplet=x64-linux-dynamic
