05ffba6118ae348df30c7b6b7dd391ab5f651c45
Andy Shapiro <shapiro.andy@epa.gov>
2025-08-07 14:29:06 -0400

    continue

--- tools/cibw_setup.sh
+++ tools/cibw_setup.sh
@@ -13,8 +13,14 @@ ls -laht /project/vcpkg_installed/x64-linux-dynamic | true
 ls -laht /project/vcpkg_installed/x64-linux-dynamic/lib | true
 ls -laht /project/vcpkg_installed/x64-linux-dynamic/include | true
 
-yum update -y
-yum install -y zip
+# yum update -y
+# yum install -y zip
 
 ./vcpkg/bootstrap-vcpkg.sh
 ./vcpkg/vcpkg install --overlay-ports=./vendor/ports --host-triplet=x64-linux-dynamic
+
+ls -laht /project | true
+ls -laht /project/vcpkg_installed/ | true
+ls -laht /project/vcpkg_installed/x64-linux-dynamic | true
+ls -laht /project/vcpkg_installed/x64-linux-dynamic/lib | true
+ls -laht /project/vcpkg_installed/x64-linux-dynamic/include | true
