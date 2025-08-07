80633a2b9f3f8e9d1e41bb84cd1a65c7b7e50f46
Andy Shapiro <shapiro.andy@epa.gov>
2025-08-07 14:15:20 -0400

    build in container

--- tools/cibw_setup.sh
+++ tools/cibw_setup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # exit if an error occurs
-set -e
+set -xe
 
 env
 
 pwd
 
-ls -laht
+ls -laht /project | true
+ls -laht /project/vcpkg_installed/ | true
+ls -laht /project/vcpkg_installed/x64-linux-dynamic | true
+ls -laht /project/vcpkg_installed/x64-linux-dynamic/lib | true
+ls -laht /project/vcpkg_installed/x64-linux-dynamic/include | true
 
+yum update -y
+yum install -y zip
 
+./vcpkg/bootstrap-vcpkg.sh"
+./vcpkg/vcpkg install --overlay-ports="${{ github.workspace }}/vendor/ports" --host-triplet=x64-linux-dynamic
