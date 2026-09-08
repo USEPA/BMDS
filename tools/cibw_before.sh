d4402500f4ea30b9497473a303c0626b759ad6d6
Cody Simmons <simmons.cody@epa.gov>
2026-09-08 18:19:51 -0400

    possible fix for vcpkg nlopt solver being omitted

--- tools/cibw_before.sh
+++ tools/cibw_before.sh
@@ -6,8 +6,20 @@ if [ "${RUNNER_OS}" = "Linux" ]; then
     yum install -y zip
 fi
 
-echo $VCPKG_HOST_TRIPLET
+# Dynamically locate vcpkg inside or outside the mapped repo root
+if [ -d "./vcpkg" ]; then
+    VCPKG_PATH="./vcpkg"
+elif [ -d "../vcpkg" ]; then
+    VCPKG_PATH="../vcpkg"
+else
+    echo "ERROR: vcpkg directory not found!"
+    exit 1
+fi
+
+echo "Found vcpkg at: $VCPKG_PATH"
+echo "Host Triplet to build: $VCPKG_HOST_TRIPLET"
+
 ./vcpkg/bootstrap-vcpkg.sh
-./vcpkg/vcpkg install --host-triplet=$VCPKG_HOST_TRIPLET
+./vcpkg/vcpkg install --host-triplet=$VCPKG_HOST_TRIPLET --x-manifest-root=.
 
 pip install pybind11==3.0.0 --target=./pybind11
